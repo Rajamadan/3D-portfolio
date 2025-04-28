3f2198824f952eb964a19accdab723f04ff39736
rajamadan <rajamadan2005@gmail.com>
2025-04-28 10:17:14 +0400

    Updated Rajamadan resume in portfolio

--- 3d-portfolio-main/vite.config.js
+++ 3d-portfolio-main/vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/3D-portfolio/', // 🔥 This must match your repo name exactly (case-sensitive)
   plugins: [react()],
-  build: {
-    sourcemap: false, // ✅ disables generation of source maps
-  },
 });
