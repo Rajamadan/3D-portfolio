a9f3a5f79a91d541b9920961ed7b07aaf8d9d660
rajamadan <rajamadan2005@gmail.com>
2025-04-28 11:12:15 +0400

    Updated CV download link

--- 3d-portfolio-main/src/components/Navbar.jsx
+++ 3d-portfolio-main/src/components/Navbar.jsx
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
-import resume from "/src/rajamadan_cv.pdf";
 import { LOGO2 } from "../assets";
+import resume from "../src/rajamadan_cv.pdf"; // Ensure correct path based on your folder structure
+
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
@@ -28,40 +29,37 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`${
-        styles.paddingX
-      } w-full flex items-center py-5 fixed top-0 z-20 ${
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 ${
         scrolled ? "backdrop-blur-sm" : "bg-transparent"
       }`}
     >
-      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
-          to='/'
-          className='flex items-center'
+          to="/"
+          className="flex items-center"
           onClick={() => {
             setActive("");
             window.scrollTo(0, 0);
           }}
         >
-          <img src={LOGO2} alt='logo' className='w-24 h-24 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer flex'>
-          Game Developer | Flutter App Developer | 3D Designer &nbsp;
-            {/* <span className='sm:block hidden'> | Portfolio</span> */}
+          <img src={LOGO2} alt="logo" className="w-24 h-24 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Game Developer | Flutter App Developer | 3D Designer &nbsp;
           </p>
         </Link>
 
         <ul className="list-none hidden sm:flex flex-row items-center gap-10">
-        <a href={resume} download="rajamadan_cv.pdf">
+          <a href={resume} download="rajamadan_cv.pdf">
             <button
               className={`${
-                active === Link.title ? "abhishek" : "abhishek-btn"
-              }   font-medium cursor-pointer border-[1px]`}
-              onClick={() => setActive(Link.title)}
+                active === "Download CV" ? "abhishek" : "abhishek-btn"
+              } font-medium cursor-pointer border-[1px]`}
+              onClick={() => setActive("Download CV")}
             >
               <span className="flex items-center animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent font-black">
                 {" "}
                 <svg
-                  class="fill-current w-4 h-4 mr-2"
+                  className="fill-current w-4 h-4 mr-2"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                 >
@@ -69,7 +67,6 @@ const Navbar = () => {
                 </svg>{" "}
                 Download CV
               </span>
-
             </button>
           </a>
           {navLinks.map((nav) => (
@@ -85,11 +82,11 @@ const Navbar = () => {
           ))}
         </ul>
 
-        <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
           <img
             src={toggle ? close : menu}
-            alt='menu'
-            className='w-[28px] h-[28px] object-contain'
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain"
             onClick={() => setToggle(!toggle)}
           />
 
@@ -98,28 +95,27 @@ const Navbar = () => {
               !toggle ? "hidden" : "flex"
             } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
           >
-            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
-            <a href={resume} download="rajamadan_cv.pdf">
-            <button
-              className={`${
-                active === Link.title ? "abhishek" : "abhishek-btn"
-              }   font-medium cursor-pointer border-[1px]`}
-              onClick={() => setActive(Link.title)}
-            >
-              <span className="flex items-center animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent font-black">
-                {" "}
-                <svg
-                  class="fill-current w-4 h-4 mr-2"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
+            <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
+              <a href={resume} download="rajamadan_cv.pdf">
+                <button
+                  className={`${
+                    active === "Download CV" ? "abhishek" : "abhishek-btn"
+                  } font-medium cursor-pointer border-[1px]`}
+                  onClick={() => setActive("Download CV")}
                 >
-                  <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-                </svg>{" "}
-                Download CV
-              </span>
-
-            </button>
-          </a>
+                  <span className="flex items-center animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent font-black">
+                    {" "}
+                    <svg
+                      className="fill-current w-4 h-4 mr-2"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+                    </svg>{" "}
+                    Download CV
+                  </span>
+                </button>
+              </a>
               {navLinks.map((nav) => (
                 <li
                   key={nav.id}
